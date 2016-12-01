1b98e448e8bc524fa3885c61eb25d0cd2d2ebd12
Maxime Zheng <maxime.zheng@epita.fr>
2016-12-01 22:18:24 +0100

    fix conflincting types, now it compile

--- create_array.h
+++ create_array.h
@@ -1,14 +1,14 @@
 // create_array.h
- 
+
 # ifndef CREATE_ARRAY_H_
 # define CREATE_ARRAY_H_
- 
+
 # include <stdlib.h>
-# include <SDL/SDL.h> 
+# include <SDL/SDL.h>
 # include <SDL.h>
-# include <SDL/SDL_image.h>                                                     
-# include <err.h>                                                               
-# include "pixel_operations.h"                                                  
+# include <SDL/SDL_image.h>
+# include <err.h>
+# include "pixel_operations.h"
 
 /*struct tab {
   int		color;
@@ -20,6 +20,6 @@
 
 int* makeArray(/*char *argv [],*/ SDL_Surface *surface);
 int** segmentation(int* array/*, char *argv []*/, SDL_Surface *surface);
-SDL_Surface* NouvelleImage(int *array, SDL_Surface* img);
+//SDL_Surface* NouvelleImage(int *array, SDL_Surface* img);
 
 # endif
